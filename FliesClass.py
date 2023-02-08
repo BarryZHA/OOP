a6bb5803d39279d9540633d1a4d652c857ca7a71
Zheng Zhang <Zheng_Zhang1@baylor.edu>
2023-02-08 15:45:46 -0600

    update

---
+++ FliesClass.py
@@ -0,0 +1,16 @@
+import random
+
+
+class Flies: 
+
+    def __init__(self): 
+        self.wings = 2 
+        self.legs = 4 
+        self.flight = 0
+
+    def launch(self): 
+        random.randint(1,10)
+
+    def getmiles(self): 
+        return self.launch
+
---
+++ Launch Flies.py
@@ -0,0 +1,12 @@
+import FliesClass as FC 
+
+def main(): 
+    house_insect = FC.Flies()
+    musquito = FC.Flies()
+    print("It can fly: ", house_insect.launch(), " miles")
+
+
+
+
+
+main() 
